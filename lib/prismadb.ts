bb0fccd00591cd0250f94926bb3bf5df1cf2024a
SNABUR <promisesimetry@gmail.com>
2025-08-19 21:12:12 -0500

    holders implementation

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -6,6 +6,10 @@ declare global {
 
 const prismadb = globalThis.prisma || new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
+  transactionOptions: {
+    maxWait: 15000, // 15 segundos
+    timeout: 15000, // 15 segundos
+  },
 })
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
